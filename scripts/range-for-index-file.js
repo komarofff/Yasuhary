c147df701c609664b6059dcb2bc628b33606e6b8
Anatoliy Komarov <admin@komaroff.biz>
2021-11-02 12:21:19 +0300

    2

--- scripts/range-for-index-file.js
+++ scripts/range-for-index-file.js
@@ -12,58 +12,67 @@
 //     $( "#amount" ).val( "¥" + $( "#slider-range" ).slider( "values", 0 ) +
 //         " - ¥" + $( "#slider-range" ).slider( "values", 1 ) );
 // } );
+function prettify(num) {
+    var n = num.toString();
+    return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+}
+
 $( function() {
     $( "#slider-range1" ).slider({
+        step: 1000,
         range: true,
         min: 1000,
         max: 100000000,
         values: [ 25000, 32000000 ],
         slide: function( event, ui ) {
-            $( "#amount1" ).html( "¥" + ui.values[ 0 ] + " - ¥" + ui.values[ 1 ] );
+            $( "#amount1" ).html( "¥ " + prettify(ui.values[ 0 ]) + " - ¥ " + prettify(ui.values[ 1 ]) );
         }
     });
-    $( "#amount1" ).html( "¥" + $( "#slider-range1" ).slider( "values", 0 ) +
-        " - ¥" + $( "#slider-range1" ).slider( "values", 1 ) );
+    $( "#amount1" ).html( "¥ " + prettify($( "#slider-range1" ).slider( "values", 0 )) +
+        " - ¥ " + prettify($( "#slider-range1" ).slider( "values", 1 )) );
 } );
 
 $( function() {
     $( "#slider-range2" ).slider({
+        step: 1000,
         range: true,
         min: 1000,
         max: 100000000,
         values: [ 25000, 32000000 ],
         slide: function( event, ui ) {
-            $( "#amount2" ).html( "¥" + ui.values[ 0 ] + " - ¥" + ui.values[ 1 ] );
+            $( "#amount2" ).html( "¥ " + prettify(ui.values[ 0 ]) + " - ¥ " + prettify(ui.values[ 1 ]) );
         }
     });
-    $( "#amount2" ).html( "¥" + $( "#slider-range2" ).slider( "values", 0 ) +
-        " - ¥" + $( "#slider-range2" ).slider( "values", 1 ) );
+    $( "#amount2" ).html( "¥" + prettify($( "#slider-range2" ).slider( "values", 0 )) +
+        " - ¥" + prettify($( "#slider-range2" ).slider( "values", 1 )) );
 } );
 
 $( function() {
     $( "#slider-range3" ).slider({
+        step: 1000,
         range: true,
         min: 1000,
         max: 100000000,
         values: [ 25000, 32000000 ],
         slide: function( event, ui ) {
-            $( "#amount3" ).html( "¥" + ui.values[ 0 ] + " - ¥" + ui.values[ 1 ] );
+            $( "#amount3" ).html( "¥ " + prettify(ui.values[ 0 ]) + " - ¥ " + prettify(ui.values[ 1 ]) );
         }
     });
-    $( "#amount3" ).html( "¥" + $( "#slider-range3" ).slider( "values", 0 ) +
-        " - ¥" + $( "#slider-range3" ).slider( "values", 1 ) );
+    $( "#amount3" ).html( "¥" + prettify($( "#slider-range3" ).slider( "values", 0 )) +
+        " - ¥" + prettify($( "#slider-range3" ).slider( "values", 1 )) );
 } );
 
 $( function() {
     $( "#slider-range4" ).slider({
+        step: 1000,
         range: true,
         min: 1000,
         max: 100000000,
         values: [ 25000, 32000000 ],
         slide: function( event, ui ) {
-            $( "#amount4" ).html( "¥" + ui.values[ 0 ] + " - ¥" + ui.values[ 1 ] );
+            $( "#amount4" ).html( "¥ " + prettify(ui.values[ 0 ]) + " - ¥ " + prettify(ui.values[ 1 ]) );
         }
     });
-    $( "#amount4" ).html( "¥" + $( "#slider-range4" ).slider( "values", 0 ) +
-        " - ¥" + $( "#slider-range4" ).slider( "values", 1 ) );
+    $( "#amount4" ).html( "¥" + prettify($( "#slider-range4" ).slider( "values", 0 )) +
+        " - ¥" + prettify($( "#slider-range4" ).slider( "values", 1 )) );
 } );
