ba7c217d568384df5bce2f25199ca2dbf2dce731
Anatoliy Komarov <admin@komaroff.biz>
2021-11-08 09:11:54 +0300

    2

---
+++ scripts/range-for-filters-file.js
@@ -0,0 +1,86 @@
+// range for tabs zone
+// $( function() {
+//     $( "#slider-range" ).slider({
+//         range: true,
+//         min: 1000,
+//         max: 100000000,
+//         values: [ 25000, 32000000 ],
+//         slide: function( event, ui ) {
+//             $( "#amount" ).val( "¥" + ui.values[ 0 ] + " - ¥" + ui.values[ 1 ] );
+//         }
+//     });
+//     $( "#amount" ).val( "¥" + $( "#slider-range" ).slider( "values", 0 ) +
+//         " - ¥" + $( "#slider-range" ).slider( "values", 1 ) );
+// } );
+function prettify(num) {
+    var n = num.toString();
+    return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
+}
+
+$( function() {
+    $( "#slider-range1" ).slider({
+        step: 1000,
+        range: true,
+        min: 1000,
+        max: 100000000,
+        values: [ 25000, 32000000 ],
+        slide: function( event, ui ) {
+            $( "#amount1" ).html( "<p class='text-xxs'>¥ " + prettify(ui.values[ 0 ]) + "</p><p class='text-xxs'> ¥ " + prettify(ui.values[ 1 ]) +"</p>" );
+            propertyPriceMin = prettify(ui.values[ 0 ])
+            propertyPriceMax = prettify(ui.values[ 1 ])
+        }
+    });
+    $( "#amount1" ).html( "<p class='text-xxs'>¥ " + prettify($( "#slider-range1" ).slider( "values", 0 )) +
+        "</p><p class='text-xxs'> ¥ " + prettify($( "#slider-range1" ).slider( "values", 1 )) +"</p>");
+    propertyPriceMin =$( "#slider-range1" ).slider( "values", 0 )
+    propertyPriceMax =$( "#slider-range1" ).slider( "values", 1 )
+
+} );
+
+
+
+function  desktopSearch() {
+
+    const popupBlocks = document.querySelectorAll('.tab-popup')
+    popupBlocks.forEach((val) => {
+        const popupDatas = val.querySelectorAll('.popup-data')
+        popupDatas.forEach((data) => {
+            data.addEventListener('click', () => {
+                for (let i = 0; i < popupDatas.length; i++) {
+                    popupDatas[i].classList.remove('text-blue-500')
+                }
+                data.classList.add('text-blue-500')
+
+                data.parentNode.parentNode.querySelector('#locations-area').innerHTML = data.dataset.title
+                data.parentNode.classList.add('hidden')
+                console.log(data.parentNode.parentNode.querySelector('.arrow-gray'),data.dataset.title)
+                // rotate arrows on tabs selects
+                let isArrow2 = data.parentNode.parentNode.querySelector('.arrow-gray')
+                if(isArrow2){
+                    isArrow2.classList.toggle('arrow-rotate')
+                }
+
+                locationProperty = val.parentNode.querySelector('.arrow-gray').dataset.name
+                if (locationProperty === 'Location') {
+                    propertyLocation = data.dataset.title
+                    propertyLocation = propertyLocation.trim()
+                    //propertyLocation = propertyLocation.replace(/\s/g, '');
+                }
+                if (locationProperty === 'Age') {
+                    propertyAge = data.innerHTML
+                    propertyAge = propertyAge.trim()
+                    //propertyAge = propertyAge.replace(/\s/g, '');
+                }
+                if (locationProperty === 'ROI') {
+                    propertyRoi = data.innerHTML
+                    propertyRoi = propertyRoi.trim()
+                    //propertyRoi = propertyRoi.replace(/\s/g, '');
+                }
+
+            })
+        })
+    })
+}
+document.addEventListener('DOMContentLoaded',()=>{
+    desktopSearch()
+})
