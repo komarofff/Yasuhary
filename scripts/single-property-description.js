5d38d221cee52e5a8343cb9fdc20e293a002e5f5
Anatoliy Komarov <admin@komaroff.biz>
2021-11-13 10:26:46 +0300

    2

--- scripts/single-property-description.js
+++ scripts/single-property-description.js
@@ -16,24 +16,48 @@ function changeSection() {
 }
 
 function showDescription() {
-    const descriptionBlocks = document.querySelector('.description-blocks').querySelectorAll('p')
-    if (descriptionBlocks.length > 0) {
-        document.querySelector('.read-more').classList.remove('hidden')
-        for (let i = 0; i < descriptionBlocks.length; i++) {
-            if (i > 0) {
-                descriptionBlocks[i].classList.add('hidden')
-            }
+    const descriptionBlocks = document.querySelector('.description-text').innerHTML
+
+    document.querySelector('.read-more').classList.remove('hidden')
+    let text = descriptionBlocks
+    let textSplit = text.split('.')
+    let message = ''
+    for(let i=0;i<textSplit.length;i++){
+        if(message.length <400 ){
+            message += `${textSplit[i]}.`
         }
     }
+    document.querySelector('.description-text').innerHTML = `${message}  `
+
 
     const readMore = document.querySelector('.read-more')
     readMore.addEventListener('click', (e) => {
         e.target.classList.add('hidden')
-        for (let i = 0; i < descriptionBlocks.length; i++) {
-            if (i > 0) {
-                descriptionBlocks[i].classList.remove('hidden')
-            }
-        }
+
+        document.querySelector('.description-text').innerHTML = `${text}.`
 
     })
 }
+
+// function showDescription() {
+//     const descriptionBlocks = document.querySelector('.description-blocks').querySelectorAll('p')
+//     if (descriptionBlocks.length > 0) {
+//         document.querySelector('.read-more').classList.remove('hidden')
+//         for (let i = 0; i < descriptionBlocks.length; i++) {
+//             if (i > 0) {
+//                 descriptionBlocks[i].classList.add('hidden')
+//             }
+//         }
+//     }
+//
+//     const readMore = document.querySelector('.read-more')
+//     readMore.addEventListener('click', (e) => {
+//         e.target.classList.add('hidden')
+//         for (let i = 0; i < descriptionBlocks.length; i++) {
+//             if (i > 0) {
+//                 descriptionBlocks[i].classList.remove('hidden')
+//             }
+//         }
+//
+//     })
+// }
