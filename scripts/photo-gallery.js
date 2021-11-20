f20f216c08f520134495e49c4e9aac1673fef564
Anatoliy Komarov <admin@komaroff.biz>
2021-11-20 11:27:49 +0300

    2

--- scripts/photo-gallery.js
+++ scripts/photo-gallery.js
@@ -52,3 +52,46 @@ function getTopPhoto() {
 
 
 }
+
+
+//open gallery block
+const galSwitchers = document.querySelectorAll('.gallery-switchers')
+let counterButtons = 0
+galSwitchers.forEach((val)=>{
+    val.addEventListener('click',(e)=>{
+        if(val.classList.contains('button-3d')){
+            start3D()
+        }else{
+            startGallery()
+        }
+    })
+})
+
+document.querySelector('.play-3d').addEventListener('click', () => {
+    start3D()
+})
+document.querySelector('.open-gallery').addEventListener('click', () => {
+    startGallery()
+})
+// close gallery block
+document.querySelector('.close-gallery-container').addEventListener('click', () => {
+    document.getElementById('gallery-block').classList.add('hidden')
+    document.getElementById('photo-block').classList.add('hidden')
+    document.getElementById('video-block').classList.add('hidden')
+})
+
+function start3D(){
+    document.getElementById('gallery-block').classList.remove('hidden')
+    document.getElementById('video-block').classList.remove('hidden')
+    document.getElementById('photo-block').classList.add('hidden')
+    document.querySelector('.button-3d').classList.add('tabs-active')
+    document.querySelector('.gallery-button').classList.remove('tabs-active')
+}
+
+function  startGallery(){
+    document.getElementById('gallery-block').classList.remove('hidden')
+    document.getElementById('photo-block').classList.remove('hidden')
+    document.getElementById('video-block').classList.add('hidden')
+    document.querySelector('.gallery-button').classList.add('tabs-active')
+    document.querySelector('.button-3d').classList.remove('tabs-active')
+}
